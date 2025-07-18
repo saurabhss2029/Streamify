84b7c0d55564bb691df80849f7fcd4877ef4b13a
Saurabh Kumar Kashinwar <skashinwar@gmail.com>
2025-07-18 13:49:05 +0530

    made some changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+// Use VITE_API_URL from your .env file set in Vercel
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
